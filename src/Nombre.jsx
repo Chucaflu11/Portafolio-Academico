542104493691e26645155a57dab3a7a0c1c62224
Rodrigo <rodrigotapia90@gmail.com>
2023-12-08 17:28:18 -0300

    route 'nombre' workin

--- src/Nombre.jsx
+++ src/Nombre.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import profesores from '../mocks/profesores'; // Ajusta la ruta según la ubicación real del archivo de mocks
+import './Nombre.css';
 
 function Nombre() {
+  const [filtroNombre, setFiltroNombre] = useState('');
+  const [profesoresList, setProfesoresList] = useState([]);
+
+  useEffect(() => {
+    // Filtra la lista de profesores solo si hay un filtro
+    if (filtroNombre.trim() !== '') {
+      const profesoresFiltrados = profesores.filter((profesor) =>
+        profesor.nombre.toLowerCase().includes(filtroNombre.toLowerCase())
+      );
+      setProfesoresList(profesoresFiltrados);
+    } else {
+      // Si no hay filtro, no mostrar ningún profesor
+      setProfesoresList([]);
+    }
+  }, [filtroNombre]);
+
   return (
     <div>
-      <h1> Nombre </h1>
-      {/* Contenido de la página */}
+      <div className="search-bar">
+        <div className="search-input-container">
+          <input
+            type="text"
+            placeholder="Buscar por nombre"
+            value={filtroNombre}
+            onChange={(e) => setFiltroNombre(e.target.value)}
+            className="search-input"
+          />
+        </div>
+      </div>
+
+      <div className="profesores-list">
+        {profesoresList.map((profesor) => (
+          <div key={profesor.id} className="profesor-card">
+            <img src={`./../mocks/images/${profesor.imagen}`} alt={profesor.nombre} />
+            <div className='data-profile'>
+              <a href={`mailto:${profesor.correo}`}>{profesor.correo}</a>
+              <br />
+              <span> Repartición: </span>
+              {profesor.reparticion}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
